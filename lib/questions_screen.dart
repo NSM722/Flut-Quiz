27877725cf4834cf5e306d9c08ee4e5592d5d5ac
Sakina Mbatha <sakina@natix.io>
2023-08-25 15:11:46 +0200

    refactor: managing + setting state

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -11,9 +11,17 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  var currentQuestionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      currentQuestionIndex++; // or currentQuestionIndex += 1
+    });
+  }
+
   @override
   Widget build(context) {
-    final currentQuestion = questions[0]; // fetching  the first question
+    final currentQuestion = questions[currentQuestionIndex];
 
     return SizedBox(
       width: double.infinity, // occupy the entire width
@@ -38,7 +46,7 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
               // map doesn't mutate the original list
               return AnswerButton(
                 answerText: answer,
-                onPress: () {},
+                onPress: answerQuestion,
               );
             }),
           ],
