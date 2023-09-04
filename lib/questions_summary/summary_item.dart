3c81f5becdd4315f1afeca417dac4dde05dc153a
Sakina Mbatha <sakina@natix.io>
2023-09-04 12:14:57 +0200

    style: bg color for answers

---
+++ lib/questions_summary/summary_item.dart
@@ -0,0 +1,60 @@
+import 'package:adv_quiz_app/questions_summary/question_identifier.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SummaryItem extends StatelessWidget {
+  const SummaryItem(this.itemData, {super.key});
+
+  final Map<String, Object> itemData;
+
+  @override
+  Widget build(BuildContext context) {
+    final isCorrectAnswer =
+        itemData['user_answer'] == itemData['correct_answer'];
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        vertical: 9,
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          QuestionIdentifier(
+            isCorrectAnswer: isCorrectAnswer,
+            questionIndex: itemData['question_index'] as int,
+          ),
+          const SizedBox(
+            width: 20,
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  itemData['question'] as String,
+                  style: GoogleFonts.alegreya(
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  itemData['user_answer'] as String,
+                  style: const TextStyle(
+                    color: Color.fromARGB(255, 190, 227, 240),
+                  ),
+                ),
+                Text(
+                  itemData['correct_answer'] as String,
+                  style: const TextStyle(
+                    color: Color.fromARGB(255, 87, 241, 100),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
